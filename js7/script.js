72d2fdd9e746e5fb611965d6860807c08bf8440e
Vladyslav Shevchenko <imvladshevchenko@gmail.com>
2020-06-17 00:53:28 +0300

    uncomment second task

--- js7/script.js
+++ js7/script.js
@@ -27,52 +27,52 @@
 
 // 2
 
-// let numb = 0;
+let numb = 0;
 
-// class Marker {
-//   constructor(options) {
-//     this.color = options.color;
-//     this.inkQty = options.inkQty;
-//     this.text = options.text;
-//     this.newInkQty = options.newInkQty;
-//   }
-//   get symbolsQty() {
-//     numb = this.inkQty / 0.5;
-//     return this.inkQty / 0.5;
-//   }
-//   printText() {
-//     if (this.text.length > this.symbolsQty) {
-//       this.text = this.text.substring(0, this.symbolsQty);
-//     }
-//     document.write(
-//       `<p style = "margin: 0; display: inline; color: ${this.color}">${this.text}</p>`
-//     );
-//   }
-//   keepPrintText() {
-//     if (this.text.length > this.newInkQty / 0.5) {
-//       this.text = this.text.substring(numb, this.newInkQty / 0.5 + numb);
-//     }
-//     document.write(
-//       `<p style = "margin: 0; display: inline; color: ${this.color}">${this.text}</p>`
-//     );
-//   }
-// }
+class Marker {
+  constructor(options) {
+    this.color = options.color;
+    this.inkQty = options.inkQty;
+    this.text = options.text;
+    this.newInkQty = options.newInkQty;
+  }
+  get symbolsQty() {
+    numb = this.inkQty / 0.5;
+    return this.inkQty / 0.5;
+  }
+  printText() {
+    if (this.text.length > this.symbolsQty) {
+      this.text = this.text.substring(0, this.symbolsQty);
+    }
+    document.write(
+      `<p style = "margin: 0; display: inline; color: ${this.color}">${this.text}</p>`
+    );
+  }
+  keepPrintText() {
+    if (this.text.length > this.newInkQty / 0.5) {
+      this.text = this.text.substring(numb, this.newInkQty / 0.5 + numb);
+    }
+    document.write(
+      `<p style = "margin: 0; display: inline; color: ${this.color}">${this.text}</p>`
+    );
+  }
+}
 
-// const marker = new Marker({
-//   color: "red",
-//   inkQty: 15,
-//   text:
-//     "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content",
-// });
-// marker.printText();
+const marker = new Marker({
+  color: "red",
+  inkQty: 15,
+  text:
+    "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content",
+});
+marker.printText();
 
-// const refillMarker = new Marker({
-//   color: "blue",
-//   newInkQty: 60,
-//   text:
-//     "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content",
-// });
-// refillMarker.keepPrintText();
+const refillMarker = new Marker({
+  color: "blue",
+  newInkQty: 60,
+  text:
+    "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content",
+});
+refillMarker.keepPrintText();
 
 // 3
 
